a25b6559088a0893cb098ac18b8c404e2405f805
ankiwoong <ankiwoong@gmail.com>
2022-06-22 20:20:24 +0900

    update

--- Chapter01/section02-1.py
+++ Chapter01/section02-1.py
@@ -38,3 +38,27 @@ print("%s's favorite number is %d" % ('Ankiwoong', 7))
 print("Test1: %5d, Price: %4.2f" % (776, 6534.123))
 print("Test1: {0: 5d}, Price: {1: 4.2f}".format(776, 6534.123))
 print("Test1: {a: 5d}, Price: {b: 4.2f}".format(a=776, b=6534.123))
+
+"""
+참고 : Escape 코드
+
+\n : 개행
+\t : 탭
+\\ : 문자
+\' : 문자
+\" : 문자
+\r : 캐리지 리턴
+\f : 폼 피드
+\a : 벨 소리
+\b : 백 스페이스
+\000 : 널 문자
+...
+
+"""
+
+print("'you'")
+print('\'you\'')
+print('"you"')
+print("""'you'""")
+print('\\you\\n\n\n\n\n\n')
+print('\t\t\ttest')
