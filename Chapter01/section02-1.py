b41b72d8637e1269776a655b7c96d5aa95133903
ankiwoong <ankiwoong@gmail.com>
2022-06-25 15:20:20 +0900

    update

--- Chapter01/section02-1.py
+++ Chapter01/section02-1.py
@@ -1,43 +1,7 @@
 # Section02-1
 # 파이썬 기초 코딩
 # Print 구문의 이해
-
-# 기본 출력
-print('Hello Python!')
-print("Hello Python!")
-print("""Hello Python!""")
-print('''Hello Python!''')
-
-print()
-
-# Separator 옵션 사용
-print('T', 'E', 'S', 'T')
-print('T', 'E', 'S', 'T', sep='')
-
-print('2019', '02', '19')
-print('2019', '02', '19', sep='-')
-
-print('niceman', 'google.com')
-print('niceman', 'google.com', sep='@')
-
-print()
-
-# end 옵션 사용
-print('Wellocome To', end='')
-print(' the black parade', end='')
-print(' piano notes')
-
-# format 사용 [], {}, ()
-print('{} and {}'.format('You', 'Me'))
-print("{0} and {1} and {0}".format('You', 'Me'))
-print("{a} are {b}".format(a='You', b='Me'))
-
-# %s : 문자 , %d : 정수 , %f : 실수
-print("%s's favorite number is %d" % ('Ankiwoong', 7))
-
-print("Test1: %5d, Price: %4.2f" % (776, 6534.123))
-print("Test1: {0: 5d}, Price: {1: 4.2f}".format(776, 6534.123))
-print("Test1: {a: 5d}, Price: {b: 4.2f}".format(a=776, b=6534.123))
+# 참조 : https://www.python-course.eu/python3_formatted_output.php
 
 """
 참고 : Escape 코드
@@ -55,10 +19,43 @@ print("Test1: {a: 5d}, Price: {b: 4.2f}".format(a=776, b=6534.123))
 ...
 
 """
+# 기본 출력
+import sys
+print('Hello Python!')  # 문법적 중요
+print("Hello Python!")  # 텍스트 의미
+print("""Hello Python!""")
+print('''Hello Python!''')
 
-print("'you'")
-print('\'you\'')
-print('"you"')
-print("""'you'""")
-print('\\you\\n\n\n\n\n\n')
-print('\t\t\ttest')
+print()
+
+# separator 옵션 사용
+print('T', 'E', 'S', 'T', sep='')
+print('2019', '02', '19', sep='-')
+print('niceman', 'google.com', sep='@')
+
+print()
+
+# end 옵션 사용
+print('Welcome To', end=' ')
+print('the black parade', end=' ')
+print('piano notes')
+
+print()
+
+# file 옵션 사용
+
+print('GeeksForGeeks', file=sys.stdout)
+
+print()
+
+print("%s's favorite number is %d" % ('Eunki', int(45)))
+
+# format 사용
+print('{} and {}'.format('You', 'Me'))
+print('{0} and {1} and {0}'.format('You', 'Me'))
+print('{var1} are {var2}'.format(var1='You', var2='Niceman'))
+
+# %d, %f, %s
+print("Test1: %5d, Price: %4.2f" % (776, 6534.123))
+print("Test1: {0:5d}, Price:{1:4.2f}".format(776, 6534.123))
+print("Test1: {a: 5d}, Price:{b: 4.2f}".format(a=776, b=6534.123))
