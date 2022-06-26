73c0c6b22659551e634d0a2f60a43551f1ed02cb
ankiwoong <ankiwoong@gmail.com>
2022-06-26 13:36:59 +0900

    update

--- Chapter01/section03.py
+++ Chapter01/section03.py
@@ -4,13 +4,13 @@
 # 외부 설치 패키지 테스트
 import simplejson as json
 
-test_dict = {'1': 95, '4': 77, '3': 65, '5': 100, '2': 88}
+test_dict = {"1": 95, "4": 77, "3": 65, "5": 100, "2": 88}
 
 # simplejson 실행
-print(json.dumps(test_dict, sort_keys=True, indent=4 * ' '))
+print(json.dumps(test_dict, sort_keys=True, indent=4 * " "))
 
 
-'''
+"""
 python -m venv 가상환경명
 	Script\activate.bat
 	Script\deactivate.bat
@@ -41,4 +41,4 @@ python -m venv 가상환경명
 	source ./deactivate : 가상환경 해제
 
 	command : code 실행
-'''
+"""
