e62afb8f95e3bf107d304a248fb2819f102e74f2
ankiwoong <ankiwoong@gmail.com>
2022-06-26 13:35:26 +0900

    update

--- Chapter01/section02-2.py
+++ Chapter01/section02-2.py
@@ -26,7 +26,7 @@ if myName == "Goodboy":
 # 반복문(구구단)
 for i in range(1, 10):
     for j in range(1, 10):
-        print('%d * %d = ' % (i, j), i * j)
+        print("%d * %d = " % (i, j), i * j)
 
 # 변수선언(한글)
 이름 = "좋은사람"
