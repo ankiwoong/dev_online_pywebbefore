1c831bc00cb047eb708792fcc9eee07fa34169fa
ankiwoong <ankiwoong@gmail.com>
2022-06-26 13:39:06 +0900

    update

--- Chapter01/section04-2.py
+++ Chapter01/section04-2.py
@@ -5,9 +5,9 @@
 
 # 문자열 생성
 str1 = "I am Boy."
-str2 = 'NiceMan'
+str2 = "NiceMan"
 str3 = """How are you?"""
-str4 = '''Thank you!'''
+str4 = """Thank you!"""
 
 # 문자열 출력
 print(type(str1))
@@ -22,7 +22,7 @@ print(len(str3))
 print(len(str4))
 
 # 빈 문자열
-str_t1 = ''
+str_t1 = ""
 str_t2 = str()
 
 print(type(str_t1), len(str_t1))
@@ -30,8 +30,8 @@ print(type(str_t2), len(str_t2))
 
 # 이스케이프 문자 사용
 
-escape_str1 = "Do you have a \"big collection\"?"
-escape_str2 = 'What\'s on TV?'
+escape_str1 = 'Do you have a "big collection"?'
+escape_str2 = "What's on TV?"
 escape_str3 = "What's on TV?"
 escape_str4 = 'This is a "book".'
 
@@ -50,7 +50,7 @@ print(t_s1)
 print(t_s2)
 
 # Raw String
-raw_s1 = r'C:\Programs\python3\"'
+raw_s1 = r"C:\Programs\python3\""
 raw_s2 = r"\\a\b\c\d"
 raw_s3 = r'\'"'
 raw_s4 = r"\"'"
@@ -61,8 +61,7 @@ print(raw_s2)
 print(raw_s3)
 print(raw_s4)
 
-multi_str1 = \
-    """
+multi_str1 = """
     문자열
     멀티라인
     테스트
@@ -70,12 +69,11 @@ multi_str1 = \
 # 멀티라인 출력
 print(multi_str1)
 
-multi_str2 = \
-    '''
+multi_str2 = """
     문자열 멀티라인 
     역슬래시(\) \
     테스트
-    '''
+    """
 # 멀티라인(역슬래시) 출력
 print(multi_str2)
 
@@ -88,10 +86,10 @@ str_o4 = "Kim Lee Park Joo"
 print(3 * str_o1)
 print(str_o1 + str_o2)
 print(dir(str_o1))
-print('x' in str_o1)
-print('i' in str_o1)
-print('e' not in str_o2)
-print('O' not in str_o2)
+print("x" in str_o1)
+print("i" in str_o1)
+print("e" not in str_o2)
+print("O" not in str_o2)
 
 # 문자열 형 변환
 print(str(77))  # type 확인
@@ -104,9 +102,9 @@ print(str(complex(12)))
 print("Capitalize: ", str_o1.capitalize())
 print("endswith?: ", str_o2.endswith("s"))
 print("join str: ", str_o1.join(["I'm ", "!"]))
-print("replace1: ", str_o1.replace('Nice', 'Good'))
+print("replace1: ", str_o1.replace("Nice", "Good"))
 print("replace2: ", str_o3.replace("is", "was", 3))
-print("split: ", str_o4.split(' '))  # Type 확인
+print("split: ", str_o4.split(" "))  # Type 확인
 print("sorted: ", sorted(str_o1))  # reverse=True
 print("reversed1: ", reversed(str_o2))  # list 형 변환
 print("reversed2: ", list(reversed(str_o2)))
@@ -123,12 +121,12 @@ for i in im_str:
 
 # 슬라이싱(인덱싱)
 # 일부분 추출(정말 중요)
-str_sl = 'Niceboy'
+str_sl = "Niceboy"
 
 # 슬라이싱 연습
 print(str_sl[0:3])
-print(str_sl[:len(str_sl)])
-print(str_sl[:len(str_sl) - 1])
+print(str_sl[: len(str_sl)])
+print(str_sl[: len(str_sl) - 1])
 print(str_sl[:])
 print(str_sl[1:4:2])
 print(str_sl[-3:6])
@@ -140,7 +138,7 @@ print(str_sl[::2])
 del str_sl
 
 # 아스키코드
-a = 't'
+a = "t"
 
 print(ord(a))
 print(chr(116))
