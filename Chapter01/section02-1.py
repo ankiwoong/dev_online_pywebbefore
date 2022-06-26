5335650e9c722063f8e8eb81e8004628c737dc29
ankiwoong <ankiwoong@gmail.com>
2022-06-26 13:35:03 +0900

    update

--- Chapter01/section02-1.py
+++ Chapter01/section02-1.py
@@ -21,39 +21,40 @@
 """
 # 기본 출력
 import sys
-print('Hello Python!')  # 문법적 중요
+
+print("Hello Python!")  # 문법적 중요
 print("Hello Python!")  # 텍스트 의미
 print("""Hello Python!""")
-print('''Hello Python!''')
+print("""Hello Python!""")
 
 print()
 
 # separator 옵션 사용
-print('T', 'E', 'S', 'T', sep='')
-print('2019', '02', '19', sep='-')
-print('niceman', 'google.com', sep='@')
+print("T", "E", "S", "T", sep="")
+print("2019", "02", "19", sep="-")
+print("niceman", "google.com", sep="@")
 
 print()
 
 # end 옵션 사용
-print('Welcome To', end=' ')
-print('the black parade', end=' ')
-print('piano notes')
+print("Welcome To", end=" ")
+print("the black parade", end=" ")
+print("piano notes")
 
 print()
 
 # file 옵션 사용
 
-print('GeeksForGeeks', file=sys.stdout)
+print("GeeksForGeeks", file=sys.stdout)
 
 print()
 
-print("%s's favorite number is %d" % ('Eunki', int(45)))
+print("%s's favorite number is %d" % ("Eunki", int(45)))
 
 # format 사용
-print('{} and {}'.format('You', 'Me'))
-print('{0} and {1} and {0}'.format('You', 'Me'))
-print('{var1} are {var2}'.format(var1='You', var2='Niceman'))
+print("{} and {}".format("You", "Me"))
+print("{0} and {1} and {0}".format("You", "Me"))
+print("{var1} are {var2}".format(var1="You", var2="Niceman"))
 
 # %d, %f, %s
 print("Test1: %5d, Price: %4.2f" % (776, 6534.123))
