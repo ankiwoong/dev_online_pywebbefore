d8063d1723d7a66f510295c658666f5168d55ddb
ankiwoong <ankiwoong@gmail.com>
2022-06-28 19:39:15 +0900

    update

--- Chapter01/section06.py
+++ Chapter01/section06.py
@@ -2,139 +2,188 @@
 # 파이썬 함수식 및 람다(lambda)
 
 # 함수 정의 방법
-# def function(parmeters):
-#   code
+# def function_name(parameter):
+#     code
 
-# 함수 호출 방법
-# function(parmeters)
-
+# 함수 호출
+# function_name()
 # 함수 선언 위치 중요
 
-# 예제 1
+
+# 예제1
 def hello(world):
-    print("hello", world)
+    print("Hello, ", world)
 
 
-hello("Python")
-hello(7777)
+param1 = "Niceman"
+
+hello(param1)
 
 
-# 예제 2
+# 예제2
 def hello_return(world):
-    val = "hello " + str(world)
-    return val
+    value = "Hello, " + str(world)
+    return value
 
 
-str = hello_return("Python!!!")
+str = hello_return("Niceman")
+
 print(str)
 
 
 # 예제3(다중리턴)
-def func_mul(x):
-    y1 = x * 100
-    y2 = x * 200
-    y3 = x * 300
+
+
+def func_mul1(x):
+    y1 = x * 2
+    y2 = x * 4
+    y3 = x * 6
     return y1, y2, y3
 
 
-var1, var2, var3 = func_mul(100)
-print(type(var1), var1, var2, var3)
+val1, val2, val3 = func_mul1(3)
+
+print(val1, val2, val3)
 
 
-# 예제4(데이터 타입 변환)
-def func_mu2l(x):
-    y1 = x * 100
-    y2 = x * 200
-    y3 = x * 300
+# 튜플 리턴
+def func_mul2(x):
+    y1 = x * 2
+    y2 = x * 4
+    y3 = x * 6
+    return (y1, y2, y3)
+
+
+tup = func_mul2(4)
+
+print(type(tup), tup, list(tup))
+
+
+# 리스트 리턴
+def func_mul2(x):
+    y1 = x * 2
+    y2 = x * 4
+    y3 = x * 6
     return [y1, y2, y3]
 
 
-lt = func_mu2l(100)
-print(lt, type(lt))
+lis = func_mul2(6)
+
+print(type(lis), lis, set(lis))
 
 
-# 예제 5
-# *args, *kwargs
+# 딕셔너리 리턴
+def func_mul3(x):
+    y1 = x * 2
+    y2 = x * 4
+    y3 = x * 6
+    return {"ret1": y1, "ret2": y2, "ret3": y3}
 
 
-def args_func(*args):
-    # for t in args:
-    #     print(t)
-    for i, v in enumerate(range(10)):
-        print(i, v)
+dic = func_mul3(8)
+
+print(type(dic), dic, dic.get("ret3"), dic.items(), dic.keys(), dic.values())
 
 
-args_func("kim")
-args_func("kim", "park")
-args_func("kim", "park", "lee")
+# 예제4
+# *args, **kwargs 이해
+
+# *args
+def args_func(*args):  # 매개변수명 자유롭게 변경 가능
+    for i, v in enumerate(args):
+        print("{}".format(i), v, end=" ")
+
+
+args_func("Kim")
+args_func("Kim", "Park")
+args_func("Kim", "Park", "Lee")
+
+print()
 
 
 # kwargs
-def kwargs_func(**kwargs):
-    for k, v in kwargs.items():
-        print(k, v)
+def kwargs_func(**kwargs):  # 매개변수명 자유롭게 변경 가능
+    for v in kwargs.keys():
+        print("{}".format(v), kwargs[v], end=" ")
 
 
-kwargs_func(name1="kim")
-kwargs_func(name1="kim", name2="park", name3="lee")
+kwargs_func(name1="Kim")
+kwargs_func(name1="Kim", name2="Park")
+kwargs_func(name1="Kim", name2="Park", name3="Lee")
+
+print()
 
 
 # 전체 혼합
-def example_mul(arg1, arg2, *args, **kwargs):
-    print(arg1, arg2, args, kwargs)
+def example(arg_1, arg_2, *args, **kwargs):
+    print(arg_1, arg_2, args, kwargs)
 
 
-example_mul(10, 20)
-example_mul(10, 20, "park", "kim", age1=24, age2=35)
+example(10, 20, "park", "kim", "lee", age1=33, age2=34, age3=44)
 
 
-# 중접함수(클로저)
+# 예제5
+# 중첩함수
 def nested_func(num):
     def func_in_func(num):
-        print(">>>>>", num)
+        print(num)
 
-    print("in func")
-    func_in_func(num + 10000)
-
-
-nested_func(10000)
+    print("In func")
+    func_in_func(num + 100)
 
 
-# 예제 6(hint)
-def func_mul3(x: int) -> list:
-    y1 = x * 100
-    y2 = x * 200
-    y3 = x * 300
-    return [y1, y2, y3]
+nested_func(1)
 
 
-print(func_mul3(5))
+# 실행불가
+# func_in_func(1)
+
+
+# 예제6
+# Hint
+def tot_length1(word: str, num: int) -> int:
+    return len(word) * num
+
+
+print("hint exam1 : ", tot_length1("i love you", 10))
+
+
+def tot_length2(word: str, num: int) -> None:
+    print("hint exam2 : ", len(word) * num)
+
+
+tot_length2("niceman", 10)
 
 
 # 람다식 예제
-# 람다식 : 메모리 절약, 가독성 향상, 코드 간결
+# 메모리 절약, 가독성 향상, 코드 간결
 # 함수는 객체 생성 -> 리소스(메모리) 할당
-# 람다는 즉시 실행(Heap 초기화) -> 메모리 초기화
+# 람다는 즉시 실행 함수(Heap 초기화) -> 메모리 초기화
+
+# 예제7
+# def mul_10(num):
+#     return num * 10
+
+# def mul_10_one(num): return num * 10
+#
+# lambda x: x * 10
 
 # 일반적 함수 -> 변수 할당
-def mul_10(num: int) -> int:
+def mul_10(num):
     return num * 10
 
 
-var_func = mul_10
-print(var_func)
-print(type(var_func))
+mul_func = mul_10
 
-print(var_func(10))
+print(mul_func(5))
+print(mul_func(6))
 
-lambda_mul_10 = lambda num: num * 10
-print(">>>>", lambda_mul_10(10))
+# 람다 함수 -> 할당
+lambda_mul_func = lambda x: x * 10
 
 
 def func_final(x, y, func):
     print(x * y * func(10))
 
 
-func_final(10, 10, lambda_mul_10)
-
-print(func_final(10, 10, lambda x: x * 1000))
+func_final(10, 10, lambda_mul_func)
