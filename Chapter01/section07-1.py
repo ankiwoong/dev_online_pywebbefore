0d9e234bd30b9562ac463c9b00adaf4e9754f9ff
ankiwoong <ankiwoong@gmail.com>
2022-06-28 19:38:57 +0900

    update

--- Chapter01/section07-1.py
+++ Chapter01/section07-1.py
@@ -4,44 +4,42 @@
 
 # 클래스, 인스턴스 차이 중요
 # 네임스페이스 : 객체를 인스턴스화 할 때 저장된 공간
-# 클래스 변수 : 직접 사용 가능, 객체 보다 먼저 생성
-# 인스턴스 변수 : 객체마다 별도 존재, 인스턴스 생성 후 사용
-
-# 선언
-# class 클래스명:
-#     함수
-#     함수
-#     함수
+# 클래스 변수 : 직접 사용 가능, 객체보다 먼저 생성
+# 인스턴스 변수 : 객체마다 별도로 존재, 인스턴스 생성 후 사용
 
 # 예제1
+
+
 class UserInfo:
-    # 속성, 메소드
-    def __init__(self, name, height, address):
+    def __init__(self, name):
         self.name = name
-        self.height = height
-        self.address = address
 
-    def user_info_p(self):
-        print("Name : ", self.name)
-        print("height : ", self.height)
-        print("address : ", self.address)
+    def print_info(self):
+        print("Name: " + self.name)
+
+    def __del__(self):
+        print("Instance removed!")
 
 
-# 네임스페이스
-user1 = UserInfo("park", 170, "서울시")
-user1.user_info_p()
-
-user2 = UserInfo("kim", 130, "대전시")
-user2.user_info_p()
+user1 = UserInfo("Kim")
+user2 = UserInfo("Park")
 
 print(id(user1))
 print(id(user2))
-print(user1.__dict__)
-print(user2.__dict__)
+
+user1.print_info()
+user2.print_info()
+
+print("user1 : ", user1.__dict__)  # 클래스 네임스페이스 확인
+print("user2 : ", user2.__dict__)
+
+print(user1.name)
 
 
 # 예제2
 # self의 이해
+
+
 class SelfTest:
     def function1():
         print("function1 called!")
@@ -51,49 +49,44 @@ class SelfTest:
         print("function2 called!")
 
 
-self_test = SelfTest()
-# self_test.function1()
-SelfTest.function1()
-self_test.function2()
+f = SelfTest()
+# print(dir(f))
+print(id(f))
+# f.function1() #예외 발생
+f.function2()
+print(SelfTest.function1())
 
-print(id(self_test))
-SelfTest.function2(self_test)
+
+# print(SelfTest.function2()) #예외 발생
 
 
 # 예제3
-# 클래스 변수, 인스턴스 변수
-class WareHouse:
+# 클래스 변수 , 인스턴스 변수
+
+
+class Warehouse:
     # 클래스 변수
     stock_num = 0
 
     def __init__(self, name):
+        # 인스턴스 변수
         self.name = name
-        WareHouse.stock_num += 1
+        Warehouse.stock_num += 1
 
     def __del__(self):
-        WareHouse.stock_num -= 1
+        Warehouse.stock_num -= 1
 
 
-user1 = WareHouse("kim")
-user2 = WareHouse("Park")
-user3 = WareHouse("Lee")
-
-print(user1.__dict__)
-print(user2.__dict__)
-print(user3.__dict__)
-print(WareHouse.__dict__)  # 클래스 네임스페이스, 클래스 변수(공유)
-
+user1 = Warehouse("Kim")
+user2 = Warehouse("Park")
 
 print(user1.name)
 print(user2.name)
-print(user3.name)
+print(user1.__dict__)
+print(user2.__dict__)
+print(Warehouse.__dict__)  # 클래스 네임스페이스 , 클래스 변수 (공유)
 
+# Warehouse.stock_num = 50 # 직접 접근 가능
 
 print(user1.stock_num)
 print(user2.stock_num)
-print(user3.stock_num)
-
-
-del user1
-print(user2.stock_num)
-print(user3.stock_num)
