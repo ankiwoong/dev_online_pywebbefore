c8649f911f35300690424bd336bb00adbd6832a1
ankiwoong <ankiwoong@gmail.com>
2022-07-24 13:24:12 +0900

    file update

--- Chapter01/section12-3.py
+++ Chapter01/section12-3.py
@@ -1,47 +1,47 @@
-# 테이블 데이터 수정 및 삭제
+# Section12-3
+# 파이썬 데이터베이스 연동(SQLite)
+# 테이블 수정 및 삭제
 
 import sqlite3
 
 # DB생성(파일)
 conn = sqlite3.connect("./Chapter01/resource/database.db")
 
-# Cursor 연결
+# Cursor연결
 c = conn.cursor()
 
 # 데이터 수정1
-# c.execute("UPDATE users SET username=? WHERE id=?", ("niceman", 2))
-# conn.commit()
+c.execute("UPDATE users SET username = ? WHERE id = ?", ("niceman", 1))
 
 # 데이터 수정2
-# c.execute("UPDATE users SET username=:name WHERE id=:id", {"name": "goodman", "id": 5})
-# conn.commit()
+c.execute(
+    "UPDATE users SET username = :name WHERE id = :id", {"name": "niceman", "id": 3}
+)
 
 # 데이터 수정3
-# c.execute("UPDATE users SET username='%s' WHERE id='%s'" % ("baddoy", 3))
-# conn.commit()
+c.execute("UPDATE users SET username = '%s' WHERE id = '%s'" % ("badboy", 5))
 
 # 중간 데이터 확인1
-# for user in c.execute("SELECT * FROM users"):
-#     print(user)
+for user in c.execute("SELECT * FROM users"):
+    print(user)
 
-# ROW Delete1
-# c.execute("DELETE FROM users WHERE id=?", (2,))
-# conn.commit()
+# Row Delete1
+c.execute("DELETE FROM users WHERE id = ?", (7,))
 
-# # ROW Delete2
-# c.execute("DELETE FROM users WHERE id=:id", {"id": 5})
-# conn.commit()
+# Row Delete2
+c.execute("DELETE FROM users WHERE id = :id", {"id": 8})
 
-# ROW Delete3
-# c.execute("DELETE FROM users WHERE id='%s'" % (4))
-# conn.commit()
+# Row Delete3
+c.execute("DELETE FROM users WHERE id = '%s'" % 9)
 
 # 중간 데이터 확인2
 for user in c.execute("SELECT * FROM users"):
     print(user)
 
 # 테이블 전체 데이터 삭제
-print("users db deleted : ", conn.execute("DELETE FROM users").rowcount, "row")
+print("users db deleted : ", conn.execute("delete from users").rowcount, "rows")
+
+# 관계형 데이터 베이스
 
 # 커밋
 conn.commit()
